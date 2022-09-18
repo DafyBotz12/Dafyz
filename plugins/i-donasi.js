806111a9c4f160cb3b75c39b275efecfc2362fb4
DafyBotz12 <reybotz104@gmail.com>
2022-09-18 13:06:38 +0700

    Dafyz

---
+++ plugins/i-donasi.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn, usedPrefix }) => { 
+      conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `
+╭─「 Donasi • Dana 」
+│ • axis  [081379753850]
+│ • Gopay  [081379753850]
+│ • DONATE LAH BANG 1000 AJA GAPAPA KOK
+╰────
+╭─「 *NOTE* 」
+│ > Ingin donasi? Wa.me/66622837764
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24jam tanpa kendala_
+╰────
+`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
+}
+handler.help = ['donasi']
+handler.tags = ['about']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
